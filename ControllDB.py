a2d2f61d4bca512bb3e482ba3a06da5b6648c14c
teriyaki398 <teriyakiisacookery@gmail.com>
2018-11-10 15:18:45 +0900

    kifu player

--- ControllDB.py
+++ ControllDB.py
@@ -1,7 +1,34 @@
 # using:utf-8
 
 from pymongo import MongoClient
+import shogi
+
 client = MongoClient('localhost', 27017)
 
+user_name = input("user_name : ")
 
-class AddBoard
+db = client["Shogiwars"]
+collection = db[user_name + "_games"]
+
+games = []
+for i in collection.find():
+    games.append(i)
+
+# 日付け順にソート
+games = sorted(games, key=lambda x:x["date"])
+
+
+board = shogi.Board()
+
+class KifuPlayer():
+
+    kifu = []
+
+    def __init__(self, kifu):
+        self.board = shogi.Board()
+        self.kifu = kifu
+
+    
+    def play(self):
+        
+
--- sample.py
+++ sample.py
@@ -1,26 +1,61 @@
+import shogi
 
-from kivy.app import App
-from kivy.uix.gridlayout import GridLayout
-from kivy.uix.label import Label
-from kivy.uix.textinput import TextInput
+def warsToSfen(kifu):
+    """
+    将棋ウォーズの棋譜をsfen形式に変換して，リストで返す
+    kifu : list<string>
+        変換したい棋譜のリスト
+    """
+    # 変換後の棋譜を格納するリスト
+    ans = []
 
-class LoginScreen(GridLayout):
+    # 棋譜をシミュレートする将棋盤
+    board = shogi.Board()
 
-    def __init__(self, **kwargs):
-        super(LoginScreen, self).__init__(**kwargs)
+    # 一手一手変換する
+    # 最後の一行は勝敗に関する情報なので取り除く
+    kifu = kifu[:-1]
+    for move in kifu:
 
-        self.cols = 2
-        self.add_widget(Label(text='User Name'))
-        self.username = TextInput(multiline=False)
-        self.add_widget(self.username)
-        self.add_widget(Label(text='password'))
-        self.password = TextInput(password=True, multiline=False)
-        self.add_widget(self.password)
+        teban       = move[0]
+        wars_from   = move[1:3]
+        wars_to     = move[3:5]
+        wars_piece  = move[5:7]
 
-class MyApp(App):
+        # リストに格納する手
+        sfen_move = ""
 
-    def build(self):
-        return LoginScreen()
+        # 打ち駒の場合
+        if wars_from == "00":
+            sfen_to     = wars_to[0]    + self.column_symbol[int(wars_to[1])]
+            
+            # 打った駒の sfenシンボルを調べる
+            piece = ""
+            if teban == "+":
+                piece = self.pieces_table[wars_piece][0]
+            elif teban == "-":
+                piece = self.pieces_table[wars_piece][1]
+            
+            sfen_move = piece + "*" + sfen_to
 
-if __name__ == "__main__":
-    MyApp().run()
+        # 駒の移動の場合
+        else:
+            sfen_from   = wars_from[0]  + self.column_symbol[int(wars_from[1])]
+            sfen_to     = wars_to[0]    + self.column_symbol[int(wars_to[1])]
+
+            # 移動後のピースが成駒かどうかを調べる
+            # もしこの移動で成っていたら成りの操作を追記する必要がある
+            if wars_piece in self.narigoma:
+                current_piece = self.retPosPiece(board, wars_from)
+                # もし既に成駒なら + がついてるはず
+                if current_piece[0] == "+":
+                    sfen_move = sfen_from + sfen_to
+                else:
+                    sfen_move = sfen_from + sfen_to + "+"
+
+        # 答えに格納し，将棋盤の手を進める
+        ans.append(sfen_move)
+        board.push(shogi.Move.from_usi(sfen_move))
+        
+    return ans
+
