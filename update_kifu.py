ecd51904c140543ed9bc50090dc95eb885746d99
teriyaki398 <teriyakiisacookery@gmail.com>
2018-11-12 12:29:16 +0900

    for gui app

---
+++ update_kifu.py
@@ -0,0 +1,34 @@
+# using:utf-8
+
+import shogi
+from tqdm import tqdm
+from pymongo import MongoClient
+from KifuPlayer import KifuPlayer
+from KifuDownloader import ShogiwarsKifuDL
+from SituationDB import SituationDB
+
+
+user_name = input("user name : ")
+rules = ["10m", "3m", "10s"]
+
+# もし新規に対局データがあればDBに追加しておく
+shogiWarsKifuDL = ShogiwarsKifuDL(user_name)
+for i in rules:
+    shogiWarsKifuDL.saveAllDataDB(i)
+
+
+# 棋譜データベースへアクセス
+client = MongoClient("localhost", 27017)
+db = client["Shogiwars"]
+games_collection = db[user_name + "_games"]
+situation_collection = db[user_name + "_situation"]
+
+games = []
+for i in games_collection.find():
+    games.append(i)
+
+situationDB = SituationDB(user_name)
+for i in tqdm(games):
+    situationDB.registerSituation(i)
+
+
